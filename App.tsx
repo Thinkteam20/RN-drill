157cc7eeff9d4a27b5025fe5936b314fea4bf699
Sangpil <tankteam20@gmail.com>
2023-06-01 15:01:12 +1000

    vector icon config issue fixed ios

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {useState} from 'react';
+import {Text, View} from 'react-native';
+import {BottomNavigation, PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-function App(): JSX.Element {
-  return (
-    <SafeAreaProvider>
-      <View>
-        <Text>Hello World!</Text>
-        <Text>Hello World!</Text>
-      </View>
-    </SafeAreaProvider>
-  );
+interface NavRoutes {
+  key: string;
+  title: string;
+  focusedIcon: string;
 }
 
-const styles = StyleSheet.create({});
+function App(): JSX.Element {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState<NavRoutes[]>([
+    {
+      key: 'chat',
+      title: 'Chats',
+      focusedIcon: 'chat',
+    },
+    {key: 'calls', title: 'Calls', focusedIcon: 'video'},
+    {key: 'people', title: 'People', focusedIcon: 'account'},
+    {key: 'stories', title: 'Stories', focusedIcon: 'book'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    chat: () => <Text>Chats</Text>,
+    calls: () => <Text>Calls</Text>,
+    people: () => <Text>People</Text>,
+    stories: () => <Text>Stories</Text>,
+  });
+
+  return (
+    <PaperProvider>
+      <SafeAreaProvider>
+        <BottomNavigation
+          navigationState={{index, routes}}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </SafeAreaProvider>
+    </PaperProvider>
+  );
+}
 
 export default App;
